f9e4afdaec64fc8cfc0cfda205345879fc08f88a
Nick Ratelle <nick.ratelle@gmail.com>
2022-12-02 13:29:28 -0500

    Update template

--- _template/project/Program.cs
+++ _template/project/Program.cs
@@ -3,30 +3,67 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using Spectre.Console;
 
 var stopwatch = Stopwatch.StartNew();
+
+var testData = 
+	(from line in File.ReadAllLines("sample.txt")
+		where !string.IsNullOrWhiteSpace(line)
+		select line).ToArray();
+
+var part1Expect = 0L;
+var part2Expect = 0L;
 
 var data =
 	(from line in File.ReadAllLines("input.txt")
 	 where !string.IsNullOrWhiteSpace(line)
 	 select line).ToArray();
 
-long GetPart1()
+
+
+long GetPart1(string[] data)
 {
 	return 0L;
 }
 
-long GetPart2()
+long GetPart2(string[] data)
 {
 	return 0L;
 }
 
-Console.WriteLine($"[{stopwatch.Elapsed}] Pre-compute");
+AnsiConsole.MarkupLineInterpolated($"[[[aqua]{stopwatch.ElapsedMilliseconds} ms[/]]] Pre-compute\n");
+
 
 stopwatch = Stopwatch.StartNew();
-var part1Result = GetPart1();
-Console.WriteLine($"[{stopwatch.Elapsed}] Part 1: {part1Result}");
+var part1TestResult = GetPart1(testData);
+AnsiConsole.MarkupLineInterpolated($"[[[aqua]{stopwatch.ElapsedMilliseconds} ms[/]]] Part 1 (Test): [aqua]{part1TestResult}[/]");
+if (part1TestResult != part1Expect)
+{
+	AnsiConsole.MarkupLineInterpolated($"[[[red]Failed[/]]] Result: [aqua]{part1TestResult}[/]  Expected: [aqua]{part1Expect}[/]");
+	return;
+}
+
+AnsiConsole.MarkupLineInterpolated($"[[[green]Passed[/]]] Result: [aqua]{part1TestResult}[/]\n");
+
 
 stopwatch = Stopwatch.StartNew();
-var part2Result = GetPart2();
-Console.WriteLine($"[{stopwatch.Elapsed}] Part 2: {part2Result}");
+var part1Result = GetPart1(data);
+AnsiConsole.MarkupLineInterpolated($"[[[aqua]{stopwatch.ElapsedMilliseconds} ms[/]]] Part 1: [aqua]{part1Result}[/]\n");
+
+
+stopwatch = Stopwatch.StartNew();
+var part2TestResult = GetPart2(testData);
+AnsiConsole.MarkupLineInterpolated($"[[[aqua]{stopwatch.ElapsedMilliseconds} ms[/]]] Part 2 (Test): [aqua]{part2TestResult}[/]");
+if (part2TestResult != part2Expect)
+{
+	AnsiConsole.MarkupLineInterpolated($"[[[red]Failed[/]]] Result: [aqua]{part2TestResult}[/]  Expected: [aqua]{part2Expect}[/]");
+	return;
+}
+
+AnsiConsole.MarkupLineInterpolated($"[[[green]Passed[/]]] Result: [aqua]{part2TestResult}[/]\n");
+
+
+stopwatch = Stopwatch.StartNew();
+var part2Result = GetPart2(data);
+AnsiConsole.MarkupLineInterpolated($"[[[aqua]{stopwatch.ElapsedMilliseconds} ms[/]]] Part 2: [aqua]{part2Result}[/]");
