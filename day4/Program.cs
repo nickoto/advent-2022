4e7f4d7df787595080711db576c4c40fc7e85478
Nick Ratelle <nick.ratelle@gmail.com>
2022-12-04 01:03:02 -0500

    Day 4

--- day4/Program.cs
+++ day4/Program.cs
@@ -11,24 +11,52 @@ var testData =
 	(from line in File.ReadAllLines("sample.txt")
 		select line).ToArray();
 
-var part1Expect = 0L;
-var part2Expect = 0L;
+var part1Expect = 2L;
+var part2Expect = 4L;
 
-var data =
+var inputData =
 	(from line in File.ReadAllLines("input.txt")
 	 select line).ToArray();
 
+(int, int) AssignmentPair(string assignment)
+{
+	var temp = assignment.Split("-");
+	return (int.Parse(temp[0]), int.Parse(temp[1]));
+}
 
+bool PairOverlapsEntirely(string pair)
+{
+	var groups = pair.Split(",");
+	var left = AssignmentPair(groups[0]);
+	var right = AssignmentPair(groups[1]);
+
+	return left.Item1 <= right.Item1 && left.Item2 >= right.Item2 ||
+	       right.Item1 <= left.Item1 && right.Item2 >= left.Item2;
+}
+
+bool PairOverlapsPartially(string pair)
+{
+	var groups = pair.Split(",");
+	var left = AssignmentPair(groups[0]);
+	var right = AssignmentPair(groups[1]);
+
+	return
+		right.Item1 >= left.Item1 && right.Item1 <= left.Item2 ||
+		left.Item1 >= right.Item1 && left.Item1 <= right.Item2;
+}
 
 long GetPart1(string[] data)
 {
-	return 0L;
+	return data
+		.Select(PairOverlapsEntirely)
+		.Count(x => x);
 }
 
 long GetPart2(string[] data)
 {
-	return 0L;
-}
+	return data
+		.Select(PairOverlapsPartially)
+		.Count(x => x);}
 
 AnsiConsole.MarkupLineInterpolated($"[[[aqua]{stopwatch.ElapsedMilliseconds} ms[/]]] Pre-compute\n");
 
@@ -46,7 +74,7 @@ AnsiConsole.MarkupLineInterpolated($"[[[green]Passed[/]]] Result: [aqua]{part1Te
 
 
 stopwatch = Stopwatch.StartNew();
-var part1Result = GetPart1(data);
+var part1Result = GetPart1(inputData);
 AnsiConsole.MarkupLineInterpolated($"[[[aqua]{stopwatch.ElapsedMilliseconds} ms[/]]] Part 1: [aqua]{part1Result}[/]\n");
 
 
@@ -63,5 +91,5 @@ AnsiConsole.MarkupLineInterpolated($"[[[green]Passed[/]]] Result: [aqua]{part2Te
 
 
 stopwatch = Stopwatch.StartNew();
-var part2Result = GetPart2(data);
+var part2Result = GetPart2(inputData);
 AnsiConsole.MarkupLineInterpolated($"[[[aqua]{stopwatch.ElapsedMilliseconds} ms[/]]] Part 2: [aqua]{part2Result}[/]");
